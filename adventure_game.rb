7f0d22a1186709fcd0626387068c19c2b5ebe5b1
Nacim Osman <nmosman2002@gmail.com>
2024-02-18 14:33:04 -0600

    Finish act 3 Done!

--- adventure_game.rb
+++ adventure_game.rb
@@ -1,17 +1,16 @@
 total_points = 0
+puts "Welcome to the Adventure Game!"
+puts "You have #{total_points} points."
+
 rooms = [
   { name: "Room 1", points: 5 },
   { name: "Room 2", points: 10 },
   { name: "Room 3", points: 15 }
 ]
 
-puts "Welcome to the Adventure Game!"
-puts "You have #{total_points} points."
-
 loop do
   puts "\nChoose a room (1-3) to enter or 'exit' to end the game:"
   input = gets.chomp
-
   case input
   when '1', '2', '3'
     total_points += rooms[input.to_i - 1][:points]
@@ -21,6 +20,6 @@ loop do
     puts "Game over! You collected a total of #{total_points} points."
     break
   else
-    puts "Invalid input. Enter '1', '2', '3', or 'exit'."
+    puts "Invalid, enter a valid room number..."
   end
 end
